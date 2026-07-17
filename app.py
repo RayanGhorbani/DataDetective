c4a4041fe941d84a7b9f80ddc2cbedd9404f7d94
Rayan <rayanghorbani13@gmail.com>
2026-07-18 00:27:34 +0330

    Upload page and connection between it and home page created.

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+
     return render_template("home.html")
+
+
+@app.route("/upload", methods=["GET" , "POST"])
+def upload():
+
+    if request.method == "POST":
+
+        dataset = request.files["dataset"]
+        print(dataset.filename)
+        
+    return render_template("upload.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
